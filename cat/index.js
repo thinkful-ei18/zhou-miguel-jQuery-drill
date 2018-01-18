c814b6d8acf0f54480772f7e81ff8a954b0641ee
Zhou <540376482yzb@gmail.com>
2018-01-18 11:11:00 -0800

    refactor

--- cat/index.js
+++ cat/index.js
@@ -1,9 +1,9 @@
 //mouse click
-$('.thumbnails').on('click','.thumbnail',e=>{
-  let myUrl = e.target.getAttribute('src');
-  let myAlt = e.target.getAttribute('alt');
-  console.log('src='+myUrl+'alt='+myAlt)
-$('.hero img').attr({'src':myUrl}).attr({'alt':myAlt});
-  
-})
+$('.thumbnails')
+  .on('click','.thumbnail',e=>{
+    let myUrl = $(e.target).find('img').getAttribute('src')
+    let myAlt = $(e.target).find('img').getAttribute('alt')
+    console.log('src='+myUrl+'alt='+myAlt)
+    $('.hero img').attr({'src':myUrl}).attr({'alt':myAlt});
+  })
 
