47ed456b6ba494895530b8231648f59462e9650c
Zhou <540376482yzb@gmail.com>
2018-01-18 13:51:01 -0800

    updated

--- cat/index.js
+++ cat/index.js
@@ -1,9 +1,9 @@
 //mouse click
-$('.thumbnails')
-  .on('click','.thumbnail',e=>{
-    let myUrl = $(e.target).find('img').getAttribute('src')
-    let myAlt = $(e.target).find('img').getAttribute('alt')
-    console.log('src='+myUrl+'alt='+myAlt)
-    $('.hero img').attr({'src':myUrl}).attr({'alt':myAlt});
-  })
+$('.thumbnail')
+.on('click', e => {
+  let myUrl = $(this).find('img').attr('src')
+  let myAlt = $(this).find('img').attr('alt')
+  console.log('src='+myUrl+'alt='+myAlt)
+  $('.hero img').attr({'src':myUrl}).attr({'alt':myAlt});
+})
 
