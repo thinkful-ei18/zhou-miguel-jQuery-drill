61aca24424ccef0a8edf9df665c1a7a0dcefa564
emptyelephants <emptyelephants@gmail.com>
2018-01-18 12:32:27 -0800

    fizz buzz on the dom

---
+++ fizzbuzz/index.js
@@ -0,0 +1,47 @@
+$('#number-chooser').submit(e=>{
+  e.preventDefault();
+ let myNum = $(e.target).find('#number-choice').val();
+ fizzBuzz(myNum);
+
+ //call fizzbuzz
+})
+
+function fizzBuzz(num){
+  let result = `<div class="fizz-buzz-item">
+                  <span></span>
+                </div>`;
+  
+
+  for(let i=0;i<=num;i++){
+  
+    
+   if(i%15===0 && i!==0){
+    result =`<div class="fizz-buzz-item fizzbuzz">
+                  <span>FizzBuzz</span>
+                </div>`;
+
+    
+  }
+  else if(i%5===0 && i!==0){
+    result =`<div class="fizz-buzz-item buzz">
+              <span>Buzz</span>
+                </div>`;
+    
+  
+  }
+  else if(i%3===0 && i!==0){
+    result =`<div class="fizz-buzz-item fizz">
+                  <span>Fizz</span>
+                </div>`;
+
+  }
+  else{
+      result =`<div class="fizz-buzz-item fizzbuzz">
+                  <span>${i}</span>
+                </div>`; 
+  }
+
+  $('.js-results').append(result);
+}
+
+}
